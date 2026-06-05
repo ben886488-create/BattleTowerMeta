18e23a770307f58ed7ee405477a3563c3a317e27
ben886488-create <ben886488@gmail.com>
2026-06-05 22:40:52 +0800

    Restore five-column sample deck grid

--- web/src/views/DeckProfile.vue
+++ web/src/views/DeckProfile.vue
@@ -8555,7 +8555,7 @@ function resetPage() {
 }
 
 .decklist-shell--sample .cardsGrid--profile {
-  grid-template-columns: repeat(4, minmax(0, 1fr));
+  grid-template-columns: repeat(5, minmax(0, 1fr));
 }
 
 .export-sample-panel {
